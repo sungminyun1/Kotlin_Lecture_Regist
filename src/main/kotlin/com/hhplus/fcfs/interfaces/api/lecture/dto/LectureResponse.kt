d67d32f1a7f2241a49ac04c06c9f40a8361cf38b
sungminyun1 <smy07207@gmail.com>
2024-12-25 23:56:01 +0900

    fix: LectureResponse 응답 변경
    응답 구조 변경

--- src/main/kotlin/com/hhplus/fcfs/interfaces/api/lecture/dto/LectureResponse.kt
+++ src/main/kotlin/com/hhplus/fcfs/interfaces/api/lecture/dto/LectureResponse.kt
@@ -3,7 +3,6 @@ package com.hhplus.fcfs.interfaces.api.lecture.dto
 import com.hhplus.fcfs.domain.lecture.dto.LectureEnrollSerRes
 
 data class LectureResponse(
-    val userId: Long,
     val lectureId: Long,
     val lectureName: String,
     val teacherName: String
@@ -11,7 +10,6 @@ data class LectureResponse(
     companion object{
         fun of(response: LectureEnrollSerRes): LectureResponse {
             return LectureResponse(
-                userId = response.userId,
                 lectureId = response.lectureId,
                 lectureName = response.lectureName,
                 teacherName = response.teacherName
